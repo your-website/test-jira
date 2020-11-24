6221a17da6e97d2d774c20702f93e7c7d2f98997
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-24 16:37:42 +0700

    finish pagination

--- src/components/PaginationList/PaginationList.js
+++ src/components/PaginationList/PaginationList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import {
   setCurrentPage,
@@ -8,54 +8,21 @@ import {
 } from "../../actions";
 
 import GithubService from "../../services/GithubService";
+import { uuid } from "../../utils";
 
 import { Button, Container, Paragraph } from "./style";
-
-const PaginationList = ({
-  currentPage,
-  githubRepo,
-  setCurrentPage,
-  setGithubRepo,
-  requestGitHubRepo,
-  showMoreRepo,
-  sortOfRepositories,
-}) => {
-  const { page, perPage } = currentPage;
-  const { totalCount } = githubRepo;
-
-  // Default one page = 30 results.
-  const maxPage = Math.ceil(totalCount / 30);
-
-  const linksPage = [];
-
-  // check the maximum number of pages received from API
-  if (page >= 3 && page < maxPage) {
-    for (let i = page - 2; i < page + 3; i++) {
-      linksPage.push(i);
-    }
-  } else if (page + 1 > maxPage) {
-    for (let i = 1; i < page + 1; i++) {
-      linksPage.push(i);
-    }
-  } else if (page + 2 > maxPage) {
-    for (let i = 1; i < page + 2; i++) {
-      linksPage.push(i);
-    }
-  } else if (page + 3 > maxPage) {
-    for (let i = 1; i < page + 3; i++) {
-      linksPage.push(i);
-    }
-  } else if (page + 4 > maxPage) {
-    for (let i = 1; i < page + 4; i++) {
-      linksPage.push(i);
-    }
-  } else {
-    for (let i = 1; i < page + 5; i++) {
-      linksPage.push(i);
-    }
-  }
-
-  async function funcData(page, perPage) {
+// = ({
+//   currentPage,
+//   githubRepo,
+//   setCurrentPage,
+//   setGithubRepo,
+//   requestGitHubRepo,
+//   showMoreRepo,
+//   sortOfRepositories,
+// })
+class PaginationList extends Component {
+  async newRepositories(page, perPage) {
+    const { requestGitHubRepo, setGithubRepo, sortOfRepositories } = this.props;
     requestGitHubRepo();
     const data = await GithubService.getRepositories(
       page,
@@ -65,59 +32,112 @@ const PaginationList = ({
     setGithubRepo(data);
   }
 
-  function setPage(action) {
+  paginationPages(page, maxPage) {
+    const linksPage = [];
+    // check the maximum number of pages received from API
+    if (page >= 3 && page < maxPage) {
+      for (let i = page - 2; i < page + 3; i++) {
+        linksPage.push(i);
+      }
+    } else if (page + 1 > maxPage) {
+      for (let i = 1; i < page + 1; i++) {
+        linksPage.push(i);
+      }
+    } else if (page + 2 > maxPage) {
+      for (let i = 1; i < page + 2; i++) {
+        linksPage.push(i);
+      }
+    } else if (page + 3 > maxPage) {
+      for (let i = 1; i < page + 3; i++) {
+        linksPage.push(i);
+      }
+    } else if (page + 4 > maxPage) {
+      for (let i = 1; i < page + 4; i++) {
+        linksPage.push(i);
+      }
+    } else {
+      for (let i = 1; i < page + 5; i++) {
+        linksPage.push(i);
+      }
+    }
+    return linksPage;
+  }
+
+  setPage(action) {
+    const { showMoreRepo, setCurrentPage } = this.props;
+    const { page, perPage } = this.props.currentPage.page;
     const updatePage = 1;
+
+    // show 10 repositories when person open new page
     showMoreRepo(updatePage);
+
+    // go to the desired page
     if (action === "next") {
       let count = page + 1;
       setCurrentPage(count);
-      funcData(count, perPage);
+      this.newRepositories(count, perPage);
     } else if (action === "prev") {
       let count = page - 1;
       setCurrentPage(count);
-      funcData(count, perPage);
+      this.newRepositories(count, perPage);
     } else {
       setCurrentPage(action);
-      funcData(action, perPage);
+      this.newRepositories(action, perPage);
     }
   }
 
-  const prevButton =
-    page === 1 ? null : <Button onClick={() => setPage("prev")}>prev</Button>;
+  render() {
+    const { page } = this.props.currentPage;
+    const { totalCount } = this.props.githubRepo;
+    const maxPage = Math.ceil(totalCount / 30);
+    const linksPage = this.paginationPages(page, maxPage);
 
-  const nextButton =
-    page > maxPage ? null : (
-      <Button onClick={() => setPage("next")}>next</Button>
-    );
+    // Default one page = 30 results.
 
-  const moreLinks = page < maxPage ? <Paragraph href="">...</Paragraph> : null;
+    const prevButton =
+      page === 1 ? null : (
+        <Button onClick={() => this.setPage("prev")}>prev</Button>
+      );
 
-  return (
-    <Container>
-      {prevButton}
-      {linksPage.map((link) => {
-        if (link === page) {
-          return (
-            <Paragraph
-              style={{ textDecoration: "underline", color: "blue" }}
-              onClick={() => setPage(link)}
-              key={link}
-            >
-              {link}
-            </Paragraph>
-          );
-        }
+    const nextButton =
+      page > maxPage ? null : (
+        <Button onClick={() => this.setPage("next")}>next</Button>
+      );
+
+    const moreLinks =
+      page < maxPage ? <Paragraph href="">...</Paragraph> : null;
+
+    const renderRepo = linksPage.map((link) => {
+      const key = uuid();
+
+      if (link === page) {
         return (
-          <Paragraph onClick={() => setPage(link)} key={link}>
+          <Paragraph
+            style={{ textDecoration: "underline", color: "blue" }}
+            onClick={() => this.setPage(link)}
+            key={key}
+          >
             {link}
           </Paragraph>
         );
-      })}
-      {moreLinks}
-      {nextButton}
-    </Container>
-  );
-};
+      }
+      return (
+        <Paragraph onClick={() => this.setPage(link)} key={key}>
+          {link}
+        </Paragraph>
+      );
+    });
+
+    return (
+      <Container>
+        {prevButton}
+        {renderRepo}
+        {moreLinks}
+        {nextButton}
+      </Container>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return state;
---
+++ src/utils/uuid.js
@@ -0,0 +1,5 @@
+function uuid() {
+  return Math.random().toString(16).slice(2);
+}
+
+export { uuid };
