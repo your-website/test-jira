beebe4af055d8fd91ca20cab140aa2a4d40894eb
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-24 12:16:28 +0700

    finish git list

--- src/components/GitList/GitList.js
+++ src/components/GitList/GitList.js
@@ -1,38 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
+import { showMoreRepo, loadingData } from "../../actions";
+
 import { Container, ButtonShow } from "./style";
 
 import GitItem from "../GitItem";
 import PaginationList from "../PaginationList";
-import { showMoreRepo, loadingData } from "../../actions";
 
-const GitList = ({ githubRepo, showMoreRepo, loadingData }) => {
-  const { data, loading, showMore } = githubRepo;
-  console.log(showMore);
-
-  async function test() {
+class GitList extends Component {
+  // maxValue no more than 30
+  showOrHideDataInPage(maxValue, value, currentValue) {
+    const { loadingData, showMoreRepo } = this.props;
+    // spinner if data loading
     loadingData();
-    if (showMore === 3) {
+    if (currentValue === maxValue) {
       return;
     }
-    const count = showMore + 1;
-    showMoreRepo(count);
+    // if value: 1 - show 10 results in page
+    // if value: 2 - show 20 results in page
+    // if value: 3 - show 30 results in page
+    showMoreRepo(value);
   }
 
-  const Button =
-    showMore < 3 ? <ButtonShow onClick={test}>Show more</ButtonShow> : null;
+  render() {
+    const { githubRepo } = this.props;
+    const { data, loading, showMore } = githubRepo;
+    // showMore - how many results to show. max 30 repPage
 
-  return (
-    <div>
-      <Container className="gitList">
-        <h3>GitList</h3>
-        <GitItem countRepo={showMore} loading={loading} data={data} />
-        {Button}
-        <PaginationList />
-      </Container>
-    </div>
-  );
-};
+    const Button =
+      showMore < 3 ? (
+        <ButtonShow
+          onClick={() => this.showOrHideDataInPage(3, showMore + 1, showMore)}
+        >
+          Show more
+        </ButtonShow>
+      ) : (
+        <ButtonShow
+          onClick={() => this.showOrHideDataInPage(1, showMore - 2, showMore)}
+        >
+          Hide
+        </ButtonShow>
+      );
+
+    return (
+      <div>
+        <Container className="gitList">
+          <h2>GitHub List Repositories</h2>
+          <p>
+            Sort of: <span>default</span>
+            <span>stars</span>
+          </p>
+          <GitItem countRepo={showMore} loading={loading} data={data} />
+          {Button}
+          <PaginationList />
+        </Container>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return state;
