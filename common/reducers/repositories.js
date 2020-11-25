893d2e76270481c3ac9ad849e2ab59aa1891e77e
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-25 15:23:58 +0700

    delete actions

--- common/reducers/repositories.js
+++ common/reducers/repositories.js
@@ -1,17 +1,11 @@
 import {
   SET_REPOSITORIES,
-  INCREMENT_COUNTER,
-  DECREMENT_COUNTER
 } from '../actions'
 
 const repositories = (state = 0, action) => {
   switch (action.type) {
     case SET_REPOSITORIES:
       return action.payload
-    case INCREMENT_COUNTER:
-      return state + 1
-    case DECREMENT_COUNTER:
-      return state - 1
     default:
       return state
   }
